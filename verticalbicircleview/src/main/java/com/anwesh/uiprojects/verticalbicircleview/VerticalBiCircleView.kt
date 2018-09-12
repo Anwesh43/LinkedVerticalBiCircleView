2e83e58b254dbe3a439cd6a2c5407fb403fc377d
Anwesh43 <anweshthecool0@gmail.com>
2018-09-13 02:40:16 +0530

    drawVBCNode canvas extension method

--- verticalbicircleview/src/main/java/com/anwesh/uiprojects/verticalbicircleview/VerticalBiCircleView.kt
+++ verticalbicircleview/src/main/java/com/anwesh/uiprojects/verticalbicircleview/VerticalBiCircleView.kt
@@ -6,12 +6,35 @@ package com.anwesh.uiprojects.verticalbicircleview
 
 import android.view.View
 import android.view.MotionEvent
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
 import android.content.Context
+import android.graphics.*
 
 val nodes : Int = 5
+
+fun Canvas.drawVBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#1976D2")
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    val gap : Float = w / (nodes + 1)
+    val r : Float = gap / 3
+    save()
+    translate(gap + i * gap, h/2)
+    for (j in 0..1) {
+        val sf = 1 - 2 * (j % 2)
+        save()
+        translate(0f, r * sf + (h/2 - 2 * r) * sf * sc2)
+        rotate(180f * sc1)
+        drawArc(RectF(-r, -r + r * j, r, -r + r * j + 2 * r),
+                180f * j, 180f, false, paint)
+        restore()
+    }
+    restore()
+}
 
 class VerticalBiCircleView(ctx : Context) : View(ctx) {
 
