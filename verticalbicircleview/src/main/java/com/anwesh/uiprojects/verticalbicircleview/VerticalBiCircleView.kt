bc6bb9ce5962daac2550c1849dee5aef825e8331
Anwesh43 <anweshthecool0@gmail.com>
2018-09-13 02:23:29 +0530

    importing libraries and define nodes

--- verticalbicircleview/src/main/java/com/anwesh/uiprojects/verticalbicircleview/VerticalBiCircleView.kt
+++ verticalbicircleview/src/main/java/com/anwesh/uiprojects/verticalbicircleview/VerticalBiCircleView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.verticalbicircleview
 /**
  * Created by anweshmishra on 13/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.content.Context
+
+val nodes : Int = 5 
