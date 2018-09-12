6571b70291d310e6f11ffa709d7d8638982e05fc
Anwesh43 <anweshthecool0@gmail.com>
2018-09-13 02:55:25 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/linkedverticalbicircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedverticalbicircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedverticalbicircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.verticalbicircleview.VerticalBiCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        VerticalBiCircleView.create(this)
     }
 }
